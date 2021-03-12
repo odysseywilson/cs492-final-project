e030f64ee97a19fe6e9e6eafca7b60ed5b6aa714
Xander <rednax825@gmail.com>
2021-03-11 17:09:07 -0800

    finished serer

--- api_server/components/database.js
+++ api_server/components/database.js
@@ -6,11 +6,10 @@ dbSchema = `
 CREATE TABLE IF NOT EXISTS Posts (
     id integer NOT NULL PRIMARY KEY,
     username text NOT NULL,
-    image text NOT NULL,
     caption text,
-    time integer NUT NULL
+    time integer NOT NULL
 );
-`
+`;
 // initialization function, connects to database and does first time setup
 var init = function (filename, next) {
     // opens connection to database, default mode is OPEN_READWRITE | CREATE
@@ -33,22 +32,18 @@ var init = function (filename, next) {
     });
 };
 
-var query = function(query, next, params = [])
-{
-    db.all(query, params, function(err, rows) {
-        if(err)
-        {
+var query = function (query, next, params = []) {
+    db.all(query, params, function (err, rows) {
+        if (err) {
             console.error(err.message);
             next(true, null);
-        }
-        else
-        {
+        } else {
             next(false, rows);
         }
     });
-}
+};
 
 module.exports = {
     init,
-    query
-}
+    query,
+};
--- api_server/server.js
+++ api_server/server.js
@@ -1,16 +1,82 @@
 const express = require("express");
+const fs = require("fs"); const multer = require("multer");
 const database = require("./components/database");
 
 var app = express();
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
-app.get("*", function(req, res, next) {
+app.get("/api/tea", function (req, res, next) {
+    query = `SELECT id, username, time FROM Posts ORDER BY time DESC LIMIT 10;`;
+    database.query(query, function (err, rows) {
+        if (err) {
+            res.status(500).send("INTERNAL SERVER ERROR");
+        } else {
+            res.status(200).json(rows);
+        }
+    });
+});
+
+app.get("/api/tea/*", function (req, res, next) {
+    var id = req.url.substring(req.url.lastIndexOf("/") + 1);
+
+    query = `SELECT * FROM Posts WHERE id = ?;`;
+    database.query(
+        query,
+        function (err, rows) {
+            if (err) {
+                res.status(500).send("INTERNAL SERVER ERROR");
+            } else {
+                res.status(200).json(rows);
+            }
+        },
+        [id]
+    );
+});
+
+app.post("/api/tea", upload.single("file"), function (req, res, next) {
+    query0 = `INSERT INTO Posts (username, caption, time) VALUES (?, ?, ?);`;
+    database.query(
+        query0,
+        function (err, rows) {
+            if (err) {
+                res.status(500).send("INTERNAL SERVER ERROR");
+            } else {
+                query1 = `SELECT last_insert_rowid();`;
+                database.query(query1, function (err, rows) {
+                    if(err)
+                    {
+                        res.status(500).send("INTERNAL SERVER ERROR");
+                    }
+                    var file =
+                        __dirname +
+                        "/public/" +
+                        rows[0]["last_insert_rowid()"] +
+                        ".png";
+                    fs.writeFile(file, req.file.buffer, function (err) {
+                        if (err) {
+                            console.error(err);
+                        res.status(500).send("INTERNAL SERVER ERROR");
+                            // TODO handle errors
+                        } else {
+                            res.status(201).end();
+                        }
+                    });
+                });
+            }
+        },
+        [req.query.username, req.query.caption, Date.now()]
+    );
+});
+
+app.get("*", function (req, res, next) {
     res.status(404).send("ERROR 404");
 });
 
-database.init("spill_the_tea.db", function() {
-    app.listen(3000, function() {
+database.init("spill_the_tea.db", function () {
+    app.listen(3000, function () {
         console.log("server listening at port: ", 3000);
     });
 });
