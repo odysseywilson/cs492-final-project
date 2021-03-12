0dc7a183c19bc216a0e36c7540c0bd03284f2159
Odyssey <odysseywilson1@gmail.com>
2021-03-12 15:02:17 -0800

    Added photo taking functionality using bitmap

--- android_app/app/src/main/java/com/example/spillthetea/MainActivity.java
+++ android_app/app/src/main/java/com/example/spillthetea/MainActivity.java
@@ -2,23 +2,111 @@ package com.example.spillthetea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     private Camera camera;
     private CameraPreview cameraPreview;
+    private File pictureFile;
+
+
+    private ImageButton takePhotoButton;
+    private Button viewTeaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Start view tea activity when clicking view tea
+        this.viewTeaButton = findViewById(R.id.view_tea_button);
+        this.viewTeaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewTeaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Take photo when capture photo button is clicked
+        this.takePhotoButton = findViewById(R.id.take_photo_button);
+        this.takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (camera != null) {
+                    //Takes the photo and then calls the Camera.getPicture method below
+                    camera.takePicture(null, null, getPicture);
+                }
+                else{
+                    System.out.println("CAMERA IS NULL");
+                }
+            }
+        });
+
+
     }
+
+    Camera.PictureCallback getPicture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            pictureFile = getOutputMediaFile();
+            System.out.println("TEST");
+            System.out.println(pictureFile);
+
+            //Open TeaPreview intent with new photo
+            Intent intent = new Intent(getBaseContext(), PreviewTea.class);
+            intent.putExtra("EXTRA_TEA_PREVIEW_PATH", pictureFile.getAbsolutePath());
+            startActivity(intent);
+
+            if (pictureFile == null) {
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found exception, photo capture failed");
+            } catch (IOException e) {
+                System.out.println("IO exception, photo capture failed");
+            }
+        }
+    };
+
+    private File getOutputMediaFile() {
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        long timestamp = System.currentTimeMillis();
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timestamp + ".jpg");
+
+        return mediaFile;
+    }
+
 
     @Override
     protected void onResume() {
@@ -26,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Launching camera");
 
         //Try to launch the camera, will fail if it is in use by another app
-        Camera camera = getCameraInstance();
+        this.camera = getCameraInstance();
 
         //Create preview and set it as the intent of this activity
         this.cameraPreview = new CameraPreview(this, camera);
--- android_app/app/src/main/java/com/example/spillthetea/PreviewTea.java
+++ android_app/app/src/main/java/com/example/spillthetea/PreviewTea.java
@@ -2,13 +2,39 @@ package com.example.spillthetea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class PreviewTea extends AppCompatActivity {
+    private Uri imageFilePath;
+    private ImageView imageView;
+    private Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview_tea);
+
+        this.imageView = findViewById(R.id.tea_preview_IV);
+        System.out.println("FILEPATH: " + getIntent().getStringExtra("EXTRA_TEA_PREVIEW_PATH"));
+        this.imageFilePath = Uri.parse(getIntent().getStringExtra("EXTRA_TEA_PREVIEW_PATH"));
+        //Uri tempURI = Uri.parse("/storage/emulated/0/Pictures/MyCameraApp/IMG_1615534786416.jpg");
+        /*Uri tempURI = this.imageFilePath;
+        this.imageView.setImageURI(tempURI);*/
+
+        try {
+            String uriString = this.imageFilePath.toString();
+            this.imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.fromFile(new File(uriString)));
+            this.imageView.setImageBitmap(this.imageBitmap);
+        } catch (IOException e) {
+            System.out.println("Error with bitmap");
+            e.printStackTrace();
+        }
     }
 }
