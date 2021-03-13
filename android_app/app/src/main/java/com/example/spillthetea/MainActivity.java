6e63831f2ba422b165afad2d73af5095d028d389
Xander <rednax825@gmail.com>
2021-03-12 20:18:44 -0800

    made snapchat share work

--- android_app/app/src/main/java/com/example/spillthetea/MainActivity.java
+++ android_app/app/src/main/java/com/example/spillthetea/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.spillthetea;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.hardware.Camera;
@@ -67,9 +68,13 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("TEST");
             System.out.println(pictureFile);
 
+            String uri = FileProvider.getUriForFile(MainActivity.this,
+                    BuildConfig.APPLICATION_ID + ".provider",
+                    pictureFile).toString();
+
             //Open TeaPreview intent with new photo
             Intent intent = new Intent(getBaseContext(), PreviewTea.class);
-            intent.putExtra("EXTRA_TEA_PREVIEW_PATH", pictureFile.getAbsolutePath());
+            intent.putExtra("EXTRA_TEA_PREVIEW_PATH", uri);
             startActivity(intent);
 
             if (pictureFile == null) {
@@ -91,10 +96,10 @@ public class MainActivity extends AppCompatActivity {
         File mediaStorageDir = new File(
                 Environment
                         .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                "MyCameraApp");
+                "SpillTheTea");
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
-                Log.d("MyCameraApp", "failed to create directory");
+                Log.d("SpillTheTea", "failed to create directory");
                 return null;
             }
         }
