071c5af9e2a3f3ea6e782b51536aa94ef7979b6b
Xander <rednax825@gmail.com>
2021-03-16 21:25:54 -0700

    added get request

--- android_app/app/src/main/java/com/example/spillthetea/ApiRepository.java
+++ android_app/app/src/main/java/com/example/spillthetea/ApiRepository.java
@@ -3,8 +3,13 @@ package com.example.spillthetea;
 import android.graphics.Bitmap;
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
@@ -18,9 +23,13 @@ public class ApiRepository {
 
     private ApiService service;
 
+    private MutableLiveData<ArrayList<TeaItem>> mTea;
+
     public ApiRepository()
     {
-        Gson gson = new GsonBuilder().create();
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(TeaItem.class, new TeaItem.JsonDeserializer())
+                .create();
 
         Retrofit retrofit = new Retrofit.Builder().
                 baseUrl("http://192.168.1.100:3000").
@@ -49,4 +58,29 @@ public class ApiRepository {
         });
     }
 
+    public void getTea()
+    {
+        Call<ArrayList<TeaItem>> results = this.service.getAllTea();
+        results.enqueue(new Callback<ArrayList<TeaItem>>() {
+
+            @Override
+            public void onResponse(Call<ArrayList<TeaItem>> call, Response<ArrayList<TeaItem>> response) {
+                if(response.code() == 200)
+                {
+                    Log.d("GET REQUEST", "call success");
+                    mTea.setValue(response.body());
+                }
+                else
+                {
+                    Log.d("GET REQUEST", "call failure");
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                Log.e("GET REQUEST", t.getMessage());
+                t.printStackTrace();
+            }
+        });
+    }
 }
